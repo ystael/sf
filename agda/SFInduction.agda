38fb2f834590d7fc55915042c93578cc06184df4
Christopher Jeris <cjeris@gmail.com>
2013-09-15 00:14:43 -0400

    almost done with Induction chapter

--- agda/SFInduction.agda
+++ agda/SFInduction.agda
@@ -2,7 +2,7 @@ module SFInduction where
 
 open import Relation.Binary.PropositionalEquality using (_≡_; refl)
 open import Basics
-open import Data.Nat
+open import Data.Nat renaming (_≤_ to leq-rel)
 
 ∧-true-elim-1 : ∀ {b c} → b ∧ c ≡ true → b ≡ true
 ∧-true-elim-1 {true}  {true}  refl = refl
@@ -45,3 +45,72 @@ double (suc n) = suc (suc (double n))
 double-+ : ∀ n → double n ≡ n + n
 double-+ zero = refl
 double-+ (suc n) rewrite double-+ n | n-+-suc-m n n = refl
+
+*-zero-+′ : ∀ n m → (0 + n) * m ≡ n * m
+*-zero-+′ n m with iden-+-left {n}
+... | refl = refl
+
++-rearrange : ∀ n m p q → (n + m) + (p + q) ≡ (m + n) + (p + q)
++-rearrange n m p q rewrite commut-+ n m = refl
+
++-swap : ∀ n m p → n + (m + p) ≡ m + (n + p)
++-swap n m p rewrite assoc-+ n m p | assoc-+ m n p | commut-+ n m = refl
+
+iden-*-right : ∀ n → n * 1 ≡ n
+iden-*-right zero = refl
+iden-*-right (suc n) rewrite iden-*-right n = refl
+
+*-suc-right : ∀ n m → n * (suc m) ≡ n + n * m
+*-suc-right zero    _ = refl
+*-suc-right (suc n) m rewrite +-swap n m (n * m) | *-suc-right n m = refl
+
+commut-* : ∀ n m → n * m ≡ m * n
+commut-* zero    m rewrite absorb-*-right m = refl
+commut-* (suc n) m rewrite *-suc-right m n | commut-* n m = refl
+
+even-suc-suc : ∀ n → even (suc (suc n)) ≡ even n
+even-suc-suc n = refl
+
+even-n-odd-suc-n : ∀ n → even n ≡ ¬ (even (suc n))
+even-n-odd-suc-n zero    = refl
+even-n-odd-suc-n (suc n) rewrite even-n-odd-suc-n n | ¬-involutive {even (suc n)} = refl
+
+≤-refl : ∀ n → n ≤ n ≡ true
+≤-refl zero    = refl
+≤-refl (suc n) rewrite ≤-refl n = refl
+
+zero-≠-suc : ∀ n → zero =ℕ= suc n ≡ false
+zero-≠-suc _ = refl
+
+∧-false-right : ∀ b → b ∧ false ≡ false
+∧-false-right true  = refl
+∧-false-right false = refl
+
+≤-left-+-compat : ∀ n m p → n ≤ m ≡ true → (p + n) ≤ (p + m) ≡ true
+≤-left-+-compat n m zero    pf = pf
+≤-left-+-compat n m (suc p) pf rewrite ≤-left-+-compat n m p pf = refl
+
+suc-≠-zero : ∀ n → suc n =ℕ= zero ≡ false
+suc-≠-zero _ = refl
+
+iden-*-left : ∀ n → 1 * n ≡ n
+iden-*-left n rewrite iden-+-right n = refl
+
+all-3-spec : ∀ b c → (b ∧ c) ∨ ((¬ b) ∨ (¬ c)) ≡ true
+all-3-spec true  true  = refl
+all-3-spec true  false = refl
+all-3-spec false true  = refl
+all-3-spec false false = refl
+
+*-+-right-distrib : ∀ n m p → (n + m) * p ≡ n * p + m * p
+*-+-right-distrib zero    m p = refl
+*-+-right-distrib (suc n) m p rewrite *-+-right-distrib n m p | assoc-+ p (n * p) (m * p) = refl
+
+assoc-* : ∀ n m p → n * (m * p) ≡ (n * m) * p
+assoc-* zero    m p = refl
+assoc-* (suc n) m p rewrite *-+-right-distrib m (n * m) p | assoc-* n m p = refl
+
+=ℕ=-refl : ∀ n → n =ℕ= n ≡ true
+=ℕ=-refl zero    = refl
+=ℕ=-refl (suc n) rewrite =ℕ=-refl n = refl
+
--- coq/Induction.v
+++ coq/Induction.v
@@ -60,3 +60,111 @@ Proof. intros n; induction n as [| n'].
 Case "n = 0".    reflexivity.
 Case "n = S n'". simpl; rewrite IHn'; rewrite plus_n_Sm; reflexivity.
 Qed.
+
+Theorem mult_0_plus' : forall n m : nat, (0 + n) * m = n * m.
+Proof. intros n m; assert (0 + n = n) as H.
+Case "H: 0 + n = n". reflexivity.
+rewrite H; reflexivity.
+Qed.
+
+Theorem plus_rearrange : forall n m p q : nat, (n + m) + (p + q) = (m + n) + (p + q).
+Proof. intros n m p q; assert (n + m = m + n) as H.
+Case "H: n + m = m + n". apply plus_comm.
+rewrite H; reflexivity.
+Qed.
+
+Theorem plus_swap : forall n m p : nat, n + (m + p) = m + (n + p).
+Proof. intros n m p; assert (n + m = m + n) as H.
+Case "H: n + m = m + n". apply plus_comm.
+rewrite plus_assoc; rewrite plus_assoc; rewrite H; reflexivity.
+Qed.
+
+Theorem mult_iden_r : forall n : nat, n * 1 = n.
+Proof. intro n; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl; rewrite IHn'; reflexivity.
+Qed.
+
+Lemma mult_S_r : forall n m : nat, n * S m = n + n * m.
+Proof. intros n m; induction n as [| n']. 
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl. rewrite plus_swap; rewrite IHn'; reflexivity.
+Qed.
+
+Theorem mult_comm : forall n m : nat, n * m = m * n.
+Proof. intros m n; induction n as [| n'].
+Case "n = 0".    simpl; apply mult_0_r.
+Case "n = S n'". simpl; rewrite mult_S_r; rewrite IHn'; reflexivity.
+Qed.
+
+Lemma evenb_SS : forall n : nat, evenb (S (S n)) = evenb n.
+Proof. reflexivity.
+Qed.
+
+Theorem evenb_n__oddb_Sn : forall n : nat, evenb n = negb (evenb (S n)).
+Proof. intros n; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". rewrite evenb_SS; rewrite IHn'; rewrite negb_involutive; reflexivity.
+Qed.
+
+Theorem ble_nat_refl : forall n : nat, ble_nat n n = true.
+Proof. intros n; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl; exact IHn'.
+Qed.
+
+Theorem zero_nbeq_S : forall n : nat, beq_nat O (S n) = false.
+Proof. reflexivity. Qed.
+
+Theorem andb_false_r : forall b : bool, andb b false = false.
+Proof. destruct b; reflexivity. Qed.
+
+Theorem plus_ble_compat_l :
+  forall n m p : nat, ble_nat n m = true -> ble_nat (p + n) (p + m) = true.
+Proof. intros n m p H; induction p as [| p'].
+Case "p = 0".    simpl; exact H.
+Case "p = S p'". simpl; exact IHp'.
+Qed.
+
+Theorem S_nbeq_0 : forall n : nat, beq_nat (S n) 0 = false.
+Proof. reflexivity. Qed.
+
+Theorem mult_iden_l : forall n : nat, 1 * n = n.
+Proof. intro n; simpl; rewrite plus_0_r; reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool, orb (andb b c) (orb (negb b) (negb c)) = true.
+Proof. intros b c; destruct b; destruct c; reflexivity.
+Qed.
+
+Theorem mult_plus_dist_r : forall n m p : nat, (n + m) * p = n * p + m * p.
+Proof. intros n m p; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl; rewrite IHn'; apply plus_assoc; reflexivity.
+Qed.
+
+Theorem mult_assoc : forall n m p : nat, n * (m * p) = (n * m) * p.
+Proof. intros n m p; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl. rewrite mult_plus_dist_r. rewrite IHn'; reflexivity.
+Qed.
+
+Theorem beq_nat_refl : forall n : nat, true = beq_nat n n.
+Proof. intro n; induction n as [| n'].
+Case "n = 0".    reflexivity.
+Case "n = S n'". simpl; exact IHn'.
+Qed.
+
+Theorem plus_swap' : forall n m p : nat, n + (m + p) = m + (n + p).
+Proof. intros n m p; repeat rewrite plus_assoc; replace (n + m) with (m + n). reflexivity.
+Case "hypothesis from replace". apply plus_comm.
+Qed.
+
+Theorem bin_to_nat_past_S : forall n : bin, bin_to_nat (bin_S n) = S (bin_to_nat n).
+Proof. intros n; induction n as [| n' | n'].
+Case "n = ZZ".    reflexivity.
+Case "n = EE n'". reflexivity.
+Case "n = OO n'". simpl; repeat rewrite plus_0_r; repeat rewrite IHn';
+  rewrite plus_n_Sm; reflexivity.
+Qed.
+
